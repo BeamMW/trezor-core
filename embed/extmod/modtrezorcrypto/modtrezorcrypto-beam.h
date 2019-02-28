4c1c2f55e7aca4e01733c12c6b8b7de9597df7dd
Kirill <gammerxpower@gmail.com>
2019-02-28 20:21:03 +0200

    BEAM: added bindings and tests for crypto functions.

--- embed/extmod/modtrezorcrypto/modtrezorcrypto-beam.h
+++ embed/extmod/modtrezorcrypto/modtrezorcrypto-beam.h
@@ -1,9 +1,62 @@
 #include "py/objstr.h"
+#include "py/objint.h"
 
 #include "beam/beam.h"
 #include "beam/functions.h"
 
+
 /// package: trezorcrypto.beam
+
+//
+#define DBG_PRINT(msg, arr, len) \
+  printf(msg);                     \
+  for (size_t i = 0; i < len; i++) \
+  {                                \
+    printf("%02x", ((int*)arr)[i]);        \
+  }                                \
+  printf("\n");
+
+// To get_uint64_t and other helper functions
+// TAKEN FROM: #include "modtrezorcrypto-monero.h"
+static uint64_t mp_obj_uint64_get_checked_beam(mp_const_obj_t self_in) {
+#if MICROPY_LONGINT_IMPL != MICROPY_LONGINT_IMPL_MPZ
+#  error "MPZ supported only"
+#endif
+
+    if (MP_OBJ_IS_SMALL_INT(self_in)) {
+        return MP_OBJ_SMALL_INT_VALUE(self_in);
+    } else {
+        byte buff[8];
+        uint64_t res = 0;
+        mp_obj_t * o = MP_OBJ_TO_PTR(self_in);
+
+        mp_obj_int_to_bytes_impl(o, true, 8, buff);
+        for (int i = 0; i<8; i++){
+            res <<= i > 0 ? 8 : 0;
+            res |= (uint64_t)(buff[i] & 0xff);
+        }
+        return res;
+    }
+}
+
+static uint64_t mp_obj_get_uint64_beam(mp_const_obj_t arg) {
+    if (arg == mp_const_false) {
+        return 0;
+    } else if (arg == mp_const_true) {
+        return 1;
+    } else if (MP_OBJ_IS_SMALL_INT(arg)) {
+        return MP_OBJ_SMALL_INT_VALUE(arg);
+    } else if (MP_OBJ_IS_TYPE(arg, &mp_type_int)) {
+        return mp_obj_uint64_get_checked_beam(arg);
+    } else {
+        if (MICROPY_ERROR_REPORTING == MICROPY_ERROR_REPORTING_TERSE) {
+            mp_raise_TypeError("can't convert to int");
+        } else {
+            nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
+                                                    "can't convert %s to int", mp_obj_get_type_str(arg)));
+        }
+    }
+}
 
 /// def hello_crypto_world() -> int:
 ///     '''
@@ -48,12 +101,210 @@ STATIC mp_obj_t mod_trezorcrypto_beam_phrase_to_seed(size_t n_args, const mp_obj
 
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorcrypto_beam_phrase_to_seed_obj, 1, 3, mod_trezorcrypto_beam_phrase_to_seed);
 
-// void generate_hash_id(uint64_t idx, uint32_t type, uint32_t sub_idx, uint8_t *out32);
+/// def generate_hash_id(idx: int, type: int, sub_idx: int, out32: bytes):
+///     '''
+///     Generate BEAM hash id.
+///     '''
+STATIC mp_obj_t mod_trezorcrypto_beam_generate_hash_id(size_t n_args, const mp_obj_t* args) {
+    uint64_t idx = mp_obj_get_uint64_beam(args[0]);
+    uint32_t type = mp_obj_get_int(args[1]);
+    uint32_t sub_idx = mp_obj_get_int(args[2]);
+
+    //uint8_t* out32 = (uint8_t*)arg_out32;
+    mp_buffer_info_t out32;
+    mp_get_buffer_raise(args[3], &out32, MP_BUFFER_RW);
+
+    generate_hash_id(idx, type, sub_idx, (uint8_t*)out32.buf);
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorcrypto_beam_generate_hash_id_obj, 4, 4, mod_trezorcrypto_beam_generate_hash_id);
+
+/// def seed_to_kdf(seed: bytes, seed_size: int, out_gen32: bytes, out_cofactor: bytes):
+///     '''
+///     Transform seed to BEAM KDF
+///     '''
+STATIC mp_obj_t mod_trezorcrypto_beam_seed_to_kdf(size_t n_args, const mp_obj_t* args) {
+    mp_buffer_info_t seed;
+    mp_get_buffer_raise(args[0], &seed, MP_BUFFER_READ);
+
+    uint64_t seed_size = mp_obj_get_uint64_beam(args[1]);
+
+    mp_buffer_info_t out_gen32;
+    mp_get_buffer_raise(args[2], &out_gen32, MP_BUFFER_RW);
+
+    mp_buffer_info_t out_cofactor;
+    mp_get_buffer_raise(args[3], &out_cofactor, MP_BUFFER_RW);
+
+    scalar_t cofactor;
+    //void seed_to_kdf(const uint8_t *seed, size_t seed_size, uint8_t *out_gen32, scalar_t *out_cof);
+    seed_to_kdf((const uint8_t*)seed.buf, seed_size, (uint8_t*)out_gen32.buf, &cofactor);
+    // Write data into out_cofactor raw pointer instead of scalar type
+    scalar_get_b32((uint8_t*)out_cofactor.buf, &cofactor);
+
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorcrypto_beam_seed_to_kdf_obj, 4, 4, mod_trezorcrypto_beam_seed_to_kdf);
+
+//void derive_key(const uint8_t *parent, uint8_t parent_size, const uint8_t *hash_id, uint8_t id_size, const scalar_t *cof_sk, scalar_t *out_res_sk);
+STATIC mp_obj_t mod_trezorcrypto_beam_derive_child_key(size_t n_args, const mp_obj_t* args) {
+    mp_buffer_info_t parent;
+    mp_get_buffer_raise(args[0], &parent, MP_BUFFER_READ);
+
+    uint8_t parent_size = mp_obj_get_int(args[1]);
+    printf("parent size!: %d\n", parent_size);
+
+    mp_buffer_info_t hash_id;
+    mp_get_buffer_raise(args[2], &hash_id, MP_BUFFER_READ);
+
+    uint8_t hash_id_size = mp_obj_get_int(args[3]);
+
+    mp_buffer_info_t cofactor_sk;
+    mp_get_buffer_raise(args[4], &cofactor_sk, MP_BUFFER_READ);
+    scalar_t cof_sk;
+    scalar_import_nnz(&cof_sk, (const uint8_t*)cofactor_sk.buf);
+
+    mp_buffer_info_t out_res_sk;
+    mp_get_buffer_raise(args[5], &out_res_sk, MP_BUFFER_RW);
+
+    scalar_t res_sk;
+    derive_key((const uint8_t*)parent.buf, parent_size, (const uint8_t*)hash_id.buf, hash_id_size, &cof_sk, &res_sk);
+
+    // Write data into out_cofactor raw pointer instead of scalar type
+    scalar_get_b32((uint8_t*)out_res_sk.buf, &res_sk);
+
+    //DEBUG_PRINT("Got res: ", (uint8_t*)out_res_sk.buf, 32)
+
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorcrypto_beam_derive_child_key_obj, 6, 6, mod_trezorcrypto_beam_derive_child_key);
+
+STATIC mp_obj_t mod_trezorcrypto_beam_secret_key_to_public_key(mp_obj_t secret_key, mp_obj_t public_key) {
+    mp_buffer_info_t sk;
+    mp_get_buffer_raise(secret_key, &sk, MP_BUFFER_READ);
+    scalar_t scalar_sk;
+    scalar_import_nnz(&scalar_sk, (const uint8_t*)sk.buf);
+
+    mp_buffer_info_t pk;
+    mp_get_buffer_raise(public_key, &pk, MP_BUFFER_RW);
+
+    //TODO<Kirill>: call it once on device initialization
+    init_context();
+    //void sk_to_pk(scalar_t *sk, const secp256k1_gej *generator_pts, uint8_t *out32);
+    sk_to_pk(&scalar_sk, CONTEXT.generator.G_pts, (uint8_t*)pk.buf);
+
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_trezorcrypto_beam_secret_key_to_public_key_obj, mod_trezorcrypto_beam_secret_key_to_public_key);
+
+STATIC mp_obj_t mod_trezorcrypto_beam_signature_sign(size_t n_args, const mp_obj_t* args) {
+    mp_buffer_info_t msg32;
+    mp_get_buffer_raise(args[0], &msg32, MP_BUFFER_READ);
+
+    mp_buffer_info_t sk;
+    mp_get_buffer_raise(args[1], &sk, MP_BUFFER_READ);
+    scalar_t scalar_sk;
+    scalar_import_nnz(&scalar_sk, (const uint8_t*)sk.buf);
+
+    // in type of point_t_x (uint8_t[32])
+    mp_buffer_info_t out_nonce_pub_x;
+    mp_get_buffer_raise(args[2], &out_nonce_pub_x, MP_BUFFER_RW);
+
+    // in type of point_t.y (uint8_t[1])
+    mp_buffer_info_t out_nonce_pub_y;
+    mp_get_buffer_raise(args[3], &out_nonce_pub_y, MP_BUFFER_RW);
+
+    mp_buffer_info_t out_k;
+    mp_get_buffer_raise(args[4], &out_k, MP_BUFFER_RW);
+    scalar_t out_scalar_k;
+
+    //TODO:move out of here
+    //secp256k1_gej pk;
+    //set_mul(&pk, CONTEXT.generator.G_pts, scalar_sk.d, 8);
+    //////
+
+    secp256k1_gej nonce;
+    //TODO<Kirill>: call it once on device initialization
+    init_context();
+    //void signature_sign(const uint8_t *msg32, const scalar_t *sk, const secp256k1_gej *generator_pts, secp256k1_gej *out_nonce_pub, scalar_t *out_k)
+    signature_sign((const uint8_t*)msg32.buf, &scalar_sk, CONTEXT.generator.G_pts, &nonce, &out_scalar_k);
+    printf(" Internal! ---- out_nonce_pub  %d\n", nonce.x.n[0]);
+    // Export scalar
+    // Write data into raw pointer instead of scalar type
+    scalar_get_b32((uint8_t*)out_k.buf, &out_scalar_k);
+
+    point_t out_k_point_t;
+    int export_result = export_gej_to_point(&nonce, &out_k_point_t);
+    if (export_result == 0)
+        mp_raise_ValueError("Invalid data length (only 16, 20, 24, 28 and 32 bytes are allowed)");
+
+    // Copy contents to out buffer
+    memcpy(out_nonce_pub_x.buf, out_k_point_t.x, 32);
+    //TODO: memset() instead?
+    *((uint8_t*)out_nonce_pub_y.buf) = out_k_point_t.y;
+
+    return mp_const_none;
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorcrypto_beam_signature_sign_obj, 5, 5, mod_trezorcrypto_beam_signature_sign);
+
+//int signature_is_valid(const uint8_t *msg32, const secp256k1_gej *nonce_pub, const scalar_t *k, const secp256k1_gej *pk, const secp256k1_gej *generator_pts);
+STATIC mp_obj_t mod_trezorcrypto_beam_is_valid_signature(size_t n_args, const mp_obj_t* args) {
+    mp_buffer_info_t msg32;
+    mp_get_buffer_raise(args[0], &msg32, MP_BUFFER_READ);
+
+    // Get nonce_pub
+    // x part
+    mp_buffer_info_t nonce_pub_x;
+    mp_get_buffer_raise(args[1], &nonce_pub_x, MP_BUFFER_READ);
+    // y part
+    mp_buffer_info_t nonce_pub_y;
+    mp_get_buffer_raise(args[2], &nonce_pub_y, MP_BUFFER_READ);
+    // Convert nonce pub from two parts to point t
+    point_t nonce_pub_point;
+    memcpy(nonce_pub_point.x, nonce_pub_x.buf, 32);
+    nonce_pub_point.y = *((int*)nonce_pub_y.buf);
+    // Convert point t to secp256k1_gej nonce pub
+    secp256k1_gej nonce_pub;
+    point_import_nnz(&nonce_pub, &nonce_pub_point);
+
+    // Get scalar k
+    mp_buffer_info_t k;
+    mp_get_buffer_raise(args[3], &k, MP_BUFFER_READ);
+    scalar_t scalar_k;
+    scalar_import_nnz(&scalar_k, (const uint8_t*)k.buf);
+
+    //!!!!!!!!!!!!!!!!
+    //TODO<Kirill>: don't accept SK as a parameter, accept parameter pk (sk after set_mul) instead
+    //!!!!!!!!!!!!!!!!
+    mp_buffer_info_t sk;
+    mp_get_buffer_raise(args[4], &sk, MP_BUFFER_READ);
+    scalar_t scalar_sk;
+    scalar_import_nnz(&scalar_sk, (const uint8_t*)sk.buf);
+    // Get pk
+    secp256k1_gej pk;
+    set_mul(&pk, CONTEXT.generator.G_pts, scalar_sk.d, 8);
+
+    int is_valid = signature_is_valid((const uint8_t*)msg32.buf, &nonce_pub, &scalar_k, &pk, CONTEXT.generator.G_pts);
+
+    return mp_obj_new_int(is_valid);
+}
+
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(mod_trezorcrypto_beam_is_valid_signature_obj, 5, 5, mod_trezorcrypto_beam_is_valid_signature);
 
 STATIC const mp_rom_map_elem_t mod_trezorcrypto_beam_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_beam) },
     { MP_ROM_QSTR(MP_QSTR_hello_crypto_world), MP_ROM_PTR(&mod_trezorcrypto_beam_hello_crypto_world_obj) },
     { MP_ROM_QSTR(MP_QSTR_phrase_to_seed), MP_ROM_PTR(&mod_trezorcrypto_beam_phrase_to_seed_obj) },
+    { MP_ROM_QSTR(MP_QSTR_generate_hash_id), MP_ROM_PTR(&mod_trezorcrypto_beam_generate_hash_id_obj) },
+    { MP_ROM_QSTR(MP_QSTR_seed_to_kdf), MP_ROM_PTR(&mod_trezorcrypto_beam_seed_to_kdf_obj) },
+    { MP_ROM_QSTR(MP_QSTR_derive_child_key), MP_ROM_PTR(&mod_trezorcrypto_beam_derive_child_key_obj) },
+    { MP_ROM_QSTR(MP_QSTR_secret_key_to_public_key), MP_ROM_PTR(&mod_trezorcrypto_beam_secret_key_to_public_key_obj) },
+    { MP_ROM_QSTR(MP_QSTR_signature_sign), MP_ROM_PTR(&mod_trezorcrypto_beam_signature_sign_obj) },
+    { MP_ROM_QSTR(MP_QSTR_is_valid_signature), MP_ROM_PTR(&mod_trezorcrypto_beam_is_valid_signature_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT (
--- src/apps/beam/display_message.py
+++ src/apps/beam/display_message.py
@@ -11,6 +11,9 @@ from apps.common.layout import *
 #TEST
 from apps.common import storage
 
+def bin_to_str(binary_data):
+    return ''.join('{:02x}'.format(x) for x in binary_data)
+
 async def display_message(ctx, msg):
     text = Text(msg.text, ui.ICON_SEND, icon_color=ui.GREEN)
     text.bold("BEAM")
@@ -25,11 +28,145 @@ async def display_message(ctx, msg):
                 #break
             else:
                 print("Processing..")
+
+    #print(dir(beam))
+    ### TEST BEAM hello_crypto_world
     #text_to_send_back = msg.text + ' This is a message from device! Received code:' + str(beam.hello_crypto_world())
+
+    ### TEST BEAM phrase_to_seed
+    #mnemonic = storage.get_mnemonic()
+    #seed = beam.phrase_to_seed(mnemonic)
+    #seed_str = ''.join('{:02x}'.format(x) for x in seed)
+    #text_to_send_back = 'Mnemonic: ' + mnemonic + '\nSeed: ' + seed_str
+
+    ### TEST BEAM generate_hash_id
+    #a_id = 123456
+    #a_type = 1113748301
+    #sub_idx = 0
+    #out32 = bytearray(32)
+    ##print(out32)
+    #beam.generate_hash_id(a_id, a_type, sub_idx, out32)
+    #out_str = ''.join('{:02x}'.format(x) for x in out32)
+    #text_to_send_back = 'Generated hash id: ' + out_str
+
+    ### TEST BEAM seed_to_kdf
+    #mnemonic = storage.get_mnemonic()
+    #seed = beam.phrase_to_seed(mnemonic)
+    #seed_size = 32
+    #out_gen32 = bytearray(32)
+    #out_cofactor = bytearray(32)
+    #beam.seed_to_kdf(seed, seed_size, out_gen32, out_cofactor)
+    #gen32_str = ''.join('{:02x}'.format(x) for x in out_gen32)
+    #cofactor_str = ''.join('{:02x}'.format(x) for x in out_cofactor)
+    #text_to_send_back = 'Gen32: {}\nCofactor: {}'.format(gen32_str, cofactor_str)
+
+    ### TEST BEAM derive_key
+    ## Get kdf
+    #mnemonic = storage.get_mnemonic()
+    #seed = beam.phrase_to_seed(mnemonic)
+    #seed_size = 32
+    #secret_key = bytearray(32)
+    #cofactor = bytearray(32)
+    #beam.seed_to_kdf(seed, seed_size, secret_key, cofactor)
+    ## Generate hash id
+    #a_id = 123456
+    #a_type = 1113748301
+    #sub_idx = 0
+    #hash_id = bytearray(32)
+    #beam.generate_hash_id(a_id, a_type, sub_idx, hash_id)
+    ## Derive key
+    #out_res_sk = bytearray(32)
+    ##print('SK: {}\nHash_id: {}'.format(bin_to_str(secret_key), bin_to_str(hash_id)))
+    #beam.derive_child_key(secret_key, 32, hash_id, 32, cofactor, out_res_sk)
+    #out_res_sk_str = bin_to_str(out_res_sk)
+    #text_to_send_back = 'Res_sk: {}'.format(out_res_sk_str)
+
+    ### TEST BEAM sk_to_pk
+    ## Get kdf
+    #mnemonic = storage.get_mnemonic()
+    #seed = beam.phrase_to_seed(mnemonic)
+    #seed_size = 32
+    #secret_key = bytearray(32)
+    #cofactor = bytearray(32)
+    #beam.seed_to_kdf(seed, seed_size, secret_key, cofactor)
+    ## Generate hash id
+    #a_id = 123456
+    #a_type = 1113748301
+    #sub_idx = 0
+    #hash_id = bytearray(32)
+    #beam.generate_hash_id(a_id, a_type, sub_idx, hash_id)
+    ## Derive key
+    #res_sk = bytearray(32)
+    #beam.derive_child_key(secret_key, 32, hash_id, 32, cofactor, res_sk)
+    ## Secret key to public key
+    #public_key = bytearray(32)
+    #beam.secret_key_to_public_key(res_sk, public_key)
+    #text_to_send_back = 'Public key: {}'.format(bin_to_str(public_key))
+
+    ### TEST BEAM signature_sign
+    ## Get kdf
+    #mnemonic = storage.get_mnemonic()
+    #seed = beam.phrase_to_seed(mnemonic)
+    #seed_size = 32
+    #secret_key = bytearray(32)
+    #cofactor = bytearray(32)
+    #beam.seed_to_kdf(seed, seed_size, secret_key, cofactor)
+    ## Generate hash id
+    #a_id = 123456
+    #a_type = 1113748301
+    #sub_idx = 0
+    #hash_id = bytearray(32)
+    #beam.generate_hash_id(a_id, a_type, sub_idx, hash_id)
+    ## Derive key
+    #res_sk = bytearray(32)
+    #beam.derive_child_key(secret_key, 32, hash_id, 32, cofactor, res_sk)
+    ## Secret key to public key
+    #public_key = bytearray(32)
+    #beam.secret_key_to_public_key(res_sk, public_key)
+    ## Signature_sign
+    #msg32 = public_key
+    #out_nonce_pub_x = bytearray(32)
+    #out_nonce_pub_y = bytearray(1)
+    #out_k = bytearray(32)
+    #beam.signature_sign(msg32, res_sk, out_nonce_pub_x, out_nonce_pub_y, out_k)
+    #text_to_send_back = 'Out nonce pub X:{}\nOut nonce pub Y:{}\nOut k:{}'.format(bin_to_str(out_nonce_pub_x),
+    #                                                                              bin_to_str(out_nonce_pub_y),
+    #                                                                              bin_to_str(out_k))
+
+    ### TEST BEAM is_signature_valid
+    # Get kdf
     mnemonic = storage.get_mnemonic()
     seed = beam.phrase_to_seed(mnemonic)
-    seed_str = ''.join('{:02x}'.format(x) for x in seed)
-    text_to_send_back = 'Mnemonic: ' + mnemonic + '\nSeed: ' + seed_str
+    seed_size = 32
+    secret_key = bytearray(32)
+    cofactor = bytearray(32)
+    beam.seed_to_kdf(seed, seed_size, secret_key, cofactor)
+    # Generate hash id
+    a_id = 123456
+    a_type = 1113748301
+    sub_idx = 0
+    hash_id = bytearray(32)
+    beam.generate_hash_id(a_id, a_type, sub_idx, hash_id)
+    # Derive key
+    res_sk = bytearray(32)
+    beam.derive_child_key(secret_key, 32, hash_id, 32, cofactor, res_sk)
+    # Secret key to public key
+    public_key = bytearray(32)
+    beam.secret_key_to_public_key(res_sk, public_key)
+    # Signature_sign
+    msg32 = public_key
+    sign_nonce_pub_x = bytearray(32)
+    sign_nonce_pub_y = bytearray(1)
+    sign_k = bytearray(32)
+    beam.signature_sign(msg32, res_sk, sign_nonce_pub_x, sign_nonce_pub_y, sign_k)
+    # Is signature valid
+    is_valid = beam.is_valid_signature(msg32, sign_nonce_pub_x, sign_nonce_pub_y, sign_k, res_sk);
+    text_to_send_back = 'Is valid signature? : {}'.format(is_valid)
+    # Modify to check the signature is no longer valid
+    msg32[0] = msg32[0] + 1
+    is_valid = beam.is_valid_signature(msg32, sign_nonce_pub_x, sign_nonce_pub_y, sign_k, res_sk);
+    text_to_send_back += '\n(xfail) Is valid signature? : {}'.format(is_valid)
+
     print("Bye!")
     res = BeamConfirmResponseMessage(text=text_to_send_back, response=True)
     print(res)
