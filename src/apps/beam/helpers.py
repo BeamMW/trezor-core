eaa80837a6ec63b59f4c56f5623e240c1d220342
Kirill <gammerxpower@gmail.com>
2019-06-18 19:08:36 +0300

    Minor imrpovements.

--- src/apps/beam/helpers.py
+++ src/apps/beam/helpers.py
@@ -17,9 +17,10 @@ def get_master_nonce_idx():
 def bin_to_str(binary_data):
     return ''.join('{:02x}'.format(x) for x in binary_data)
 
-def get_beam_kdf():
-    # Get kdf
-    mnemonic = storage.get_mnemonic()
+def get_beam_kdf(mnemonic=None):
+    if mnemonic is None:
+        # Get kdf
+        mnemonic = storage.get_mnemonic()
     seed = beam.from_mnemonic_beam(mnemonic)
     seed_size = 32
     secret_key = bytearray(32)
@@ -27,24 +28,29 @@ def get_beam_kdf():
     beam.seed_to_kdf(seed, seed_size, secret_key, cofactor)
     return (secret_key, cofactor)
 
-def get_beam_sk(kid_idx, kid_sub_idx=0):
+def get_beam_sk(kid_idx, kid_sub_idx=0, kdf=None):
     # Generate hash id
     kid_type = BBS_KEY()
     hash_id = bytearray(32)
     beam.generate_hash_id(kid_idx, kid_type, kid_sub_idx, hash_id)
-    # Get kdf
-    secret_key, cofactor = get_beam_kdf()
+
+    if kdf is None:
+        # Get kdf
+        secret_key, cofactor = get_beam_kdf()
+    else:
+        secret_key, cofactor = kdf
+
     # Derive key
     res_sk = bytearray(32)
     beam.derive_child_key(secret_key, 32, hash_id, 32, cofactor, res_sk)
 
     return res_sk
 
-def get_beam_pk(kid_idx, kid_sub_idx=0):
+def get_beam_pk(kid_idx, kid_sub_idx=0, kdf=None):
     # Secret key to public key
     public_key_x = bytearray(32)
     public_key_y = bytearray(1)
-    sk = get_beam_sk(kid_idx, kid_sub_idx)
+    sk = get_beam_sk(kid_idx, kid_sub_idx, kdf)
     beam.secret_key_to_public_key(sk, public_key_x, public_key_y)
 
     return (public_key_x, int(public_key_y[0]))
