b45a1f5bac49bfb65567160fc635f9643870a56a
Kirill <gammerxpower@gmail.com>
2019-06-18 20:05:20 +0300

    Bug fix.

--- src/apps/beam/generate_key.py
+++ src/apps/beam/generate_key.py
@@ -15,6 +15,6 @@ async def generate_key(ctx, msg):
 
     beam.generate_key(msg.kidv.idx, msg.kidv.type, msg.kidv.sub_idx, msg.kidv.value,
                       msg.is_coin_key, seed,
-                      key_image_x, key_image_x)
+                      key_image_x, key_image_y)
 
     return BeamECCPoint(x=key_image_x, y=int(key_image_y[0]))
