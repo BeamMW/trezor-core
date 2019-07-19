54657e69dfdf4890b8430d7dc92e7a1e6072273c
vldmkr <vladmakaryan@gmail.com>
2019-07-19 17:29:03 +0300

    fixes after merge

--- src/apps/beam/nonce.py
+++ src/apps/beam/nonce.py
@@ -15,12 +15,14 @@ def create_master_nonce(seed):
 
 def is_master_nonce_created():
     master_nonce = config.get(beam_app_id(), get_master_nonce_idx())
-    return len(master_nonce) > 0
+    return master_nonce is not None
 
 def create_nonce(idx):
     if idx != get_master_nonce_idx():
         old_nonce = config.get(beam_app_id(), idx)
-        new_nonce = bytearray(old_nonce) if len(old_nonce) > 0 else bytearray(32)
+        new_nonce = bytearray(32)
+        if old_nonce:
+                new_nonce = bytearray(old_nonce)
         master_nonce = config.get(beam_app_id(), get_master_nonce_idx())
         beam.create_derived_nonce(master_nonce, idx, new_nonce)
         config.set(beam_app_id(), idx, new_nonce)
